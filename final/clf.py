ab772476df663aa5c2b6982e896cb46ad1f5ddfe
KVR Aditya <kvraditya577@gmail.com>
2021-04-15 11:28:41 +0530

    added normalized data(min max scaling)

--- final/clf.py
+++ final/clf.py
@@ -4,5 +4,5 @@ from read_data import *
 
 for i in range(1, 57):
     file_name = f'../data/{i}.csv'
-    df, data = read_data(file_name, clf=True)
-    np.savetxt(f"../new_data/n{i}.csv", data, delimiter=",")
+    df, data = read_data(file_name, clf=True, norm=True)
+    np.savetxt(f"../normalized_data/n{i}.csv", data, delimiter=",")
